fd628f640bc0f205ab8412dd8f5a3ad6439df891
BoviliusMeidi <boviliusbm@gmail.com>
2025-05-12 22:23:19 +0700

    feat : about page

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Products from '@/components/Products';
+
+export default function AboutPage() {
+    return (
+        <div>
+            <p>About Page</p>
+            <h1>Welcome to PharmaCheck</h1>
+            <Products />
+        </div>
+    )
+}
