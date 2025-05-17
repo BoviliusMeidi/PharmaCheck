18de0f1c026529ae5be3f4f621888eea816ce45e
BoviliusMeidi <boviliusbm@gmail.com>
2025-05-17 17:44:35 +0700

    feat : config for supabase bucket images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns: [new URL('https://fymbxhctbdfclafdndmn.supabase.co/**')]
+    }
+};
 
 export default nextConfig;
