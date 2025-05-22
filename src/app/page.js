f24b6661df66f7929c44e0491fa1df3b5aad5ece
BoviliusMeidi <boviliusbm@gmail.com>
2025-05-22 21:12:45 +0700

    change : search form params

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import SearchForm from '@/components/SearchForm';
 export default function HomePage ()  {
   return (
     <div className='max-w-4xl'>
-      <SearchForm title={"Search"}/>
+      <SearchForm title={"Search"} objective={"product"}/>
     </div>
   );
 };
