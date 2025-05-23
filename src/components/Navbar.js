857dc2a5c0049ce2674379eef0bde6c47ba079e8
justinedeclans@gmail.com <justinedeclans@gmail.com>
2025-05-23 22:38:57 +0700

    refactor: navbar navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="fixed top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent z-50">
+    <header className="fixed top-0 left-0 w-full flex justify-between items-center py-4 px-4 bg-transparent z-50">
       <div className="flex items-center">
         <img
           src="/logo.png"
@@ -10,16 +10,16 @@ export default function Navbar() {
       </div>
 
       <nav className="flex justify-around items-center w-[685px] h-[70px] rounded-[40px] bg-white shadow-md flex-shrink-0">
-        <a href="#" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
+        <a href="/" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
           Home
         </a>
-        <a href="#" className="flex justify-center items-center w-[71px] h-[30px] text-[#004E64] border-b-4 border-[#004E64] text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
+        <a href="/about" className="flex justify-center items-center w-[71px] h-[30px] text-[#004E64] border-b-4 border-[#004E64] text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
           About
         </a>
-        <a href="#" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
+        <a href="/verify" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
           Verify
         </a>
-        <a href="#" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
+        <a href="/categories" className="flex justify-center items-center w-[71px] h-[30px] text-black text-[24px] font-description font-bold leading-normal no-underline flex-shrink-0">
           Categories
         </a>
       </nav>
