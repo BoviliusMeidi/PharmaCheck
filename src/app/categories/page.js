23f9bc8e6b6b029cc362ef9bbb168524ef8e06d1
justinedeclans@gmail.com <justinedeclans@gmail.com>
2025-05-24 09:57:08 +0700

    feat: image link categories

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 
 export default function CategoriesPage() {
@@ -7,14 +8,18 @@ export default function CategoriesPage() {
             <div className="h-screen flex-1 py-12 px-8 max-w-[1083px] mx-auto mt-24 ">
                 <div className='font-title text-center text-[80px]'>Medicine Categories</div>
                 <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 gap-40">
-                    <div className="w-[476px] h-[441px] rounded-3xl rounded-lg shadow-lg bg-[#F4F4F4] p-6 text-center">
-                        <h2 className="text-[50px] font-description font-bold mb-10">Medicine</h2>
-                        <img src="/medicine.png" alt="Medicine" className="w-[130px] h-[260px] mx-auto h-40" />
-                    </div>
-                    <div className="w-[476px] h-[441px] rounded-3xl shadow-lg bg-[#F4F4F4] p-6 text-center">
-                        <h2 className="text-[50px] font-description font-bold mb-10">Non Medicine</h2>
-                        <img src="/medicine.png" alt="Non Medicine" className="w-[130px] h-[260px] mx-auto h-40" />
-                    </div>
+                    <Link href="/categories/medicine">
+                        <div className="cursor-pointer w-[476px] h-[441px] rounded-3xl shadow-lg bg-[#F4F4F4] p-6 text-center hover:shadow-xl transition-shadow">
+                            <h2 className="text-[50px] font-description font-bold mb-10">Medicine</h2>
+                            <img src="/medicine.png" alt="Medicine" className="w-[130px] h-[260px] mx-auto" />
+                        </div>
+                    </Link>
+                    <Link href="/categories/non-medicine">
+                        <div className="cursor-pointer w-[476px] h-[441px] rounded-3xl shadow-lg bg-[#F4F4F4] p-6 text-center hover:shadow-xl transition-shadow">
+                            <h2 className="text-[50px] font-description font-bold mb-10">Non Medicine</h2>
+                            <img src="/medicine.png" alt="Non Medicine" className="w-[130px] h-[260px] mx-auto" />
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
