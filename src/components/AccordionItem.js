9edf007bfcc3b1e36c1b40c28a999f99fdf606b0
BoviliusMeidi <boviliusbm@gmail.com>
2025-06-02 14:10:45 +0700

    style : make responsive accordion item

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -7,13 +7,18 @@ export default function AccordionItem({ title, children }) {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className="border-b border-black py-2 mx-36">
+        <div className="border-b border-black py-2 px-4 sm:px-12 md:mx-20 lg:mx-36">
             <button
-                className="flex justify-between items-center w-full text-left font-header text-lg cursor-pointer"
+                className="flex justify-between items-center w-full text-left font-header cursor-pointer"
                 onClick={() => setIsOpen(!isOpen)}
             >
-                <span className='font-header text-5xl'>{title}</span>
-                {isOpen ? <ChevronUp size={80} /> : <ChevronDown size={80} />}
+                <span className="font-header text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+                    {title}
+                </span>
+                {isOpen
+                    ? <ChevronUp className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16" />
+                    : <ChevronDown className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16" />
+                }
             </button>
             {isOpen && (
                 <div className="mt-2 text-sm font-description">
